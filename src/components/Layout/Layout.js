76b8a22c3b3f129db75a8f5aaf77aa62c2a66d0d
Tomas Mejia <tomasme_lu@lagash.com>
2018-08-19 13:01:21 -0300

    Changed Layout to stateful component and set state

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
-const layout = ( props ) => (
-	<React.Fragment>
-		<Toolbar />
-		<SideDrawer />
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</React.Fragment>
-);
+class Layout extends Component { 
+	state = {
+		showSideDrawer: true
+	}
+	sideDrawerCloseHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+	render() {
+		return(
+			<React.Fragment>
+				<Toolbar />
+				<SideDrawer 
+				open={this.state.showSideDrawer} 
+				closed={this.sideDrawerCloseHandler} />
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</React.Fragment>
+		)
+	}
+}
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import classes from './SideDrawer.css';
 
 const SideDrawer = props => {
-    // ...
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
   return (
-    <div className={classes.SideDrawer}>
-        <Logo height="11%" mb="32px" />
-        <nav>
-            <NavItems />
-        </nav>
-    </div>
+      <React.Fragment>
+          <Backdrop show={props.open} clicked={props.closed} />
+          <div className={attachedClasses.join(" ")}>
+              <Logo height="11%" mb="32px" />
+              <nav>
+                  <NavItems />
+              </nav>
+          </div>
+      </React.Fragment>
   );
 };
 

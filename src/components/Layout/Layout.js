0b392708997f0253f8ee09ff1fe3d4191e9b128e
Tomas Mejia <tomasme_lu@lagash.com>
2018-08-19 12:39:11 -0300

    SideDrawer implemented

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import classes from './Layout.css';
-
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+import classes from './Layout.css';
 
 const layout = ( props ) => (
 	<React.Fragment>
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>
 			{props.children}
 		</main>

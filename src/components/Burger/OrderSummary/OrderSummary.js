77e744fe3d13fe47ccd7b55cafdee41eefcddbb9
Tomas Mejia <tomasme_lu@lagash.com>
2018-08-18 21:41:22 -0300

    Initial commit

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const OrderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li><span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}</li> 
+        });
+  return (
+    <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients: </p>
+        <ul>
+            {ingredientSummary}
+        </ul>
+        <p>Continue to checkout?</p>
+    </React.Fragment>
+  );
+}
+
+export default OrderSummary;
